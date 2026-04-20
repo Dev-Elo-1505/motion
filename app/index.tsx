21735228af1d21edab475a4f42d863665aade65f
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2026-04-20 13:55:01 +0100

    chore: setup up project and nativewind

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+
+
+export default function Index() {
+  return (
+    <View
+      className="bg-green-500 p-5"
+    >
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.yyyy</Text>
+    </View>
+  );
+}
